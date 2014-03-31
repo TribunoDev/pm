afd2dc0cd7460d42129fe45aeaf9b20e9c520d48
mjbc007 <mjbc007@gmail.com>
2014-03-31 16:01:12 -0600

    Modifica el admin de Productos

--- web/admin.py
+++ web/admin.py
@@ -14,6 +14,7 @@ class DetalleImgAdmin(admin.ModelAdmin):
 
 
 class ProductoAdmin(admin.ModelAdmin):
+	search_fields = ['Descripcion']
 	ordering = ['Descripcion']
 
 class Editor(admin.ModelAdmin):
